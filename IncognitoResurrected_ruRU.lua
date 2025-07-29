a5c5c668e507705f7af4df6a61774c4422aec4dd
Starlynk <info@starlynk.net>
2025-07-29 07:41:39 -0700

    Fix for Mists not working with versioning checks

--- IncognitoResurrected_ruRU.lua
+++ IncognitoResurrected_ruRU.lua
@@ -1,6 +1,6 @@
 ------------------------
 ---		Version      ---
----		 1.2.3       ---
+---		 1.2.4       ---
 ------------------------
 
 local L = LibStub("AceLocale-3.0"):NewLocale("IncognitoResurrected", "ruRU", true)
